e458db3fa3df21ff4002e39ce3a2963655842afb
SA-Shaon <u1903047@student.cuet.ac.bd>
2024-06-29 17:26:34 +0600

    product updated

--- src/prices.js
+++ src/prices.js
@@ -6,27 +6,27 @@ export const prices = [
   },
   {
     _id: 1,
-    name: "$0 to 19",
-    array: [0, 19],
+    name: "$0 to 1999",
+    array: [0, 1999],
   },
   {
     _id: 2,
-    name: "$20 to 39",
-    array: [20, 39],
+    name: "$2000 to 3999",
+    array: [2000, 3999],
   },
   {
     _id: 3,
-    name: "$40 to 59",
-    array: [0, 19],
+    name: "$4000 to 5999",
+    array: [4000, 5999],
   },
   {
     _id: 4,
-    name: "$60 to 79",
-    array: [0, 19],
+    name: "$6000 to 7999",
+    array: [6000, 7999],
   },
   {
     _id: 5,
-    name: "More than $80",
-    array: [80, 9999],
+    name: "More than $8000",
+    array: [8000, 99999999999999],
   },
 ];
